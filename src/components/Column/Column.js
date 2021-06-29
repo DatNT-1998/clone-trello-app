d4c1e1adef47d763213d02f89d6cc77fd7d7ed1b
DatNT2-D1 <datnt2@fabbi.io>
2021-06-29 16:56:54 +0700

    Implement drag and drop

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,23 +1,42 @@
 import React from 'react';
 import Card from 'components/Card/Card';
+import { Container, Draggable } from 'react-smooth-dnd';
 
-import './Column.scss';
 import { mapOrder } from 'utilities/sort';
+import './Column.scss';
 
 const Column = (props) => {
   const { column } = props;
-  console.log(column, 'column');
 
   const cards = mapOrder(column.cards, column.cardOrder, 'id');
 
+  const onCardDrop = (dropResult) => {
+    console.log(dropResult);
+  };
   return (
     <div className='column'>
-      <header>{column.title}</header>
-      <ul className='card-list'>
-        {cards.map((card, index) => (
-          <Card key={index} card={card} />
-        ))}
-      </ul>
+      <header className='column-drag-handle'>{column.title}</header>
+      <div className='card-list'>
+        <Container
+          groupName='col'
+          onDrop={onCardDrop}
+          getChildPayload={(index) => cards[index]}
+          dragClass='card-ghost'
+          dropClass='card-ghost-drop'
+          dropPlaceholder={{
+            animationDuration: 150,
+            showOnTop: true,
+            className: 'card-drop-preview',
+          }}
+          dropPlaceholderAnimationDuration={200}
+        >
+          {cards.map((card, index) => (
+            <Draggable key={index}>
+              <Card card={card} />
+            </Draggable>
+          ))}
+        </Container>
+      </div>
       <footer>Add another card</footer>
     </div>
   );
