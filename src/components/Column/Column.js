32bcae0ec28e67769bf107248737b1ab32229a57
DatNT2-D1 <datnt2@fabbi.io>
2021-07-02 17:28:42 +0700

    edit remove column

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,18 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Card from 'components/Card/Card';
 import { Container, Draggable } from 'react-smooth-dnd';
 
 import { mapOrder } from 'utilities/sort';
 import './Column.scss';
+import { Dropdown, Form } from 'react-bootstrap';
+import ConfirmModal from 'components/Common/ConfirmModal';
 
 const Column = (props) => {
   const { column, onCardDrop } = props;
+  const [showConfirmModal, setShowConfirmModal] = useState(false);
 
   const cards = mapOrder(column.cards, column.cardOrder, 'id');
 
+  const toggleOpenConfirmModal = () => setShowConfirmModal(!showConfirmModal);
+
+  const onConfirmModalAction = (type) => {
+    console.log(type);
+    toggleOpenConfirmModal();
+  };
+
   return (
     <div className='column'>
-      <header className='column-drag-handle'>{column.title}</header>
+      <header className='column-drag-handle'>
+        <div className='column-title'>
+          <Form.Control
+            size='sm'
+            type='text'
+            placeholder='Enter column title ...'
+            className='input-contenteditable'
+            // ref={newColumnInputRef}
+            // onChange={handleTitleChange}
+            value={column.title}
+          />
+        </div>
+        <div className='column-dropdown-actions'>
+          <Dropdown>
+            <Dropdown.Toggle
+              id='dropdown-basic'
+              size='sm'
+              className='dropdown-btn'
+            />
+
+            <Dropdown.Menu>
+              <Dropdown.Item>Add card...</Dropdown.Item>
+              <Dropdown.Item onClick={toggleOpenConfirmModal}>
+                Remove column...
+              </Dropdown.Item>
+              <Dropdown.Item>
+                Move all cards in this column(beta)...
+              </Dropdown.Item>
+              <Dropdown.Item>
+                Archive all cards in this column(beta)...
+              </Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        </div>
+      </header>
       <div className='card-list'>
         <Container
           groupName='col'
@@ -39,6 +83,12 @@ const Column = (props) => {
           <i className='fa fa-plus'>Add another card</i>
         </div>
       </footer>
+      <ConfirmModal
+        show={showConfirmModal}
+        title='Remove Column'
+        content={`Are you sure to delete ${column.title}. All related Cards will be remove`}
+        onAction={onConfirmModalAction}
+      />
     </div>
   );
 };
